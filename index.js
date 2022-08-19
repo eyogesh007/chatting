9edaa8af5d28f220c9874acd153eab289bdb34bd
Yogesh <yogesh@Shivendras-MacBook-Air.local>
2022-08-19 15:26:00 +0530

    chatting app commit

--- index.js
+++ index.js
@@ -77,6 +77,31 @@ app.get('/username',checktoken,async(req,res)=>{
     res.send(exist.username);
 })
 
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
 app.listen(5000,()=>{
     console.log('server running')
 })
